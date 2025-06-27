f0a67ec0c146cc122dc1cf169289ad3f031d86e5
maxin <mx01853368@lazada.com>
2025-06-27 09:56:19 +0800

    feat: 1

--- learn-admin/vite.config.ts
+++ learn-admin/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+  },
   server: {
     port: 5173, // Vite默认端口
     proxy: {
